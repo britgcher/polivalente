d8e9a80d31b76d17773686bf59a3c311cc6f7c74
britgcher <bn922084@gmail.com>
2023-11-28 16:30:45 -0300

    ggg

--- src/app/receita/receita.page.spec.ts
+++ src/app/receita/receita.page.spec.ts
@@ -5,11 +5,11 @@ describe('ReceitaPage', () => {
   let component: ReceitaPage;
   let fixture: ComponentFixture<ReceitaPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
     fixture = TestBed.createComponent(ReceitaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
