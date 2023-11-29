c52eaaf0fe10ec40fd81bc6ebaf51df060070f14
britgcher <bn922084@gmail.com>
2023-11-28 22:57:45 -0300

    bbbb

--- src/app/semacucar/semacucar.module.ts
+++ src/app/semacucar/semacucar.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { SemacucarPageRoutingModule } from './semacucar-routing.module';
-
 import { SemacucarPage } from './semacucar.page';
+import { HeaderModule } from '../header/header.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SemacucarPageRoutingModule
+    SemacucarPageRoutingModule,
+    HeaderModule,
   ],
   declarations: [SemacucarPage]
 })
